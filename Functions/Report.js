78b49cabbc1bbf80ae3268f8857feb3933259f79
Tulio Graciano <tuliobeorgr@gmail.com>
2026-08-08 14:39:31 -0300

    Graphs
    Addition of graph to show readings with charts

--- Functions/Report.js
+++ Functions/Report.js
@@ -1,4 +1,5 @@
 
+import asciichart from 'asciichart';
 export function Report(readings, result){
     console.log("-------- READINGS REPORT --------");
     console.log(`Total Readings: ${result.Total}`);
@@ -33,4 +34,30 @@ export function Report(readings, result){
             console.log(`- Pressure: ${alert.Pressure}, Timestamp: ${alert.Timestamp}`);
         });
     }
+console.log("\n-------- TEMPERATURE CHART --------\n");
+const temperaturas = readings.map(r => r.temperatura);
+const HigherLimit = 80;
+const LowerLimit = 10;
+const Max = temperaturas.map(() => HigherLimit);
+const Min = temperaturas.map(() => LowerLimit);
+
+console.log(asciichart.plot(
+    [temperaturas, Max, Min],
+    {
+        height: 10,
+    }
+));
+    console.log("\n-------- PRESSURE CHART --------\n");
+    const pressoes = readings.map(r => r.pressao);
+    const HigherLimitPress = 1050;
+    const LowerLimitPress = 980;
+    const MaxPress = pressoes.map(() => HigherLimitPress);
+    const MinPress = pressoes.map(() => LowerLimitPress);
+    console.log(asciichart.plot(
+        [pressoes, MaxPress, MinPress],
+        {
+            height: 10,
+        }
+    ));
 }
+
