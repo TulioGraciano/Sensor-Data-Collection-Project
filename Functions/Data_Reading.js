3536393ed3d560e3d8902dcbe4e92a2053ff65b4
Tulio Graciano <tuliobeorgr@gmail.com>
2026-08-08 13:44:59 -0300

    Correção das funções Get_Data e Data_Reading
    Correção das funções com mudança de nomes de variaveis

--- Functions/Data_Reading.js
+++ Functions/Data_Reading.js
@@ -1,6 +1,6 @@
 
 // Function to read data and classify it into alert, normal, and critical categories based on temperature and pressure thresholds
-function ReadData(data){
+export function ReadData(readings){
     //Process Control Parameters
     let TempMinAllowed = 10;
     let TempMaxAllowed = 80;
@@ -14,25 +14,25 @@ function ReadData(data){
     let pressAlert = [];
     let tempCritical = [];
     let pressCritical = [];
-    for(let i = 0; i < data.length; i++){
+    for(let i = 0; i < readings.length; i++){
         switch(true){
-            case (data[i].temperatura <TempMinAllowed || data[i].pressao <PressMinAllowed):
+            case (readings[i].temperatura <TempMinAllowed || readings[i].pressao <PressMinAllowed):
                 alert++;
-                if(data[i].temperatura <TempMinAllowed){
-                    tempAlert.push({Temperature: data[i].temperatura, Timestamp: data[i].timestamp});
+                if(readings[i].temperatura <TempMinAllowed){
+                    tempAlert.push({Temperature: readings[i].temperatura, Timestamp: readings[i].timestamp});
                 }else{
-                    pressAlert.push({Pressure: data[i].pressao, Timestamp: data[i].timestamp});
+                    pressAlert.push({Pressure: readings[i].pressao, Timestamp: readings[i].timestamp});
                 }
                 break;
-            case (data[i].temperatura >TempMinAllowed && data[i].temperatura <TempMaxAllowed && data[i].pressao >PressMinAllowed && data[i].pressao <PressMaxAllowed):
+            case (readings[i].temperatura >TempMinAllowed && readings[i].temperatura <TempMaxAllowed && readings[i].pressao >PressMinAllowed && readings[i].pressao <PressMaxAllowed):
                 normal++;
                 break;
-            case (data[i].temperatura >TempMaxAllowed || data[i].pressao >PressMaxAllowed):
+            case (readings[i].temperatura >TempMaxAllowed || readings[i].pressao >PressMaxAllowed):
                 critical++;
-                if(data[i].temperatura >TempMaxAllowed){
-                    tempCritical.push({Temperature: data[i].temperatura, Timestamp: data[i].timestamp});
+                if(readings[i].temperatura >TempMaxAllowed){
+                    tempCritical.push({Temperature: readings[i].temperatura, Timestamp: readings    [i].timestamp});
                 }else{
-                    pressCritical.push({Pressure: data[i].pressao, Timestamp: data[i].timestamp});
+                    pressCritical.push({Pressure: readings[i].pressao, Timestamp: readings[i].timestamp});
                 }
                 break;
         }
